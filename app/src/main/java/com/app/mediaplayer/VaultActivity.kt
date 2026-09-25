ad676eee0b304e56b54e01e7f42e0384e0662f5e
anshsaini8445 <anshsaini8445@gmail.com>
2026-09-25 08:39:51 +0530

    Create VaultActivity.kt

---
+++ app/src/main/java/com/app/mediaplayer/VaultActivity.kt
@@ -0,0 +1,12 @@
+package com.app.mediaplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VaultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Linking to the clean XML layout
+        setContentView(R.layout.activity_vault)
+    }
+}
